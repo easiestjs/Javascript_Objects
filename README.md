eac9e0fe919b7ac921b40d6830d886a49ff08385
SpringSrikanth <srikanthyenagagandula89@gmail.com>
2022-06-03 12:05:18 +0530

    Added new example in index.js

--- README.md
+++ README.md
@@ -25,18 +25,44 @@
   console.log(typeof person);
   console.log(person)
 
-  /** We can add the properties dynamically by using person.favColor ='white' */
-  person.favColor= "white" 
-  /** OR */
-  /** We can add the properties dynamically by using person["favColor"] ='white' */
+  /** We can add the properties dynamically by using person.favColor ='white'  or person["favColor"] ='white' */
+  /** person.favColor= "white" */
   person["favColor"]= "yellow" 
 
-  /** We can get the specific property by using person.fistName */
+  /** We can get the specific property by using person.fistName or person["fistName"]  */
   console.log(person.firstName) 
-
-  /** OR */
-  /** We can get the specific property by using person["fistName"] */
   console.log(person["firstName"]) 
   console.log(person)
+
+  /** To delete properties from object use delete person.favColor or delete person["favColor"] */
+  /** delete person.favColor */
+  delete person["favColor"]
+  console.log(person)
+
+```
+
+
+### Example (2)
+
+```bash
+const person2 ={
+    firstName : 'Koseksi',
+    lastName :'P',
+    gender: 'Male',
+    height:'5.7',
+    fullName(){
+        console.log(`${this.firstName} ${this.lastName}`);
+    }
+}
+
+console.log(person2)
+person2.fullName();
+
+/** We can add methods from outside also by using reference type */
+person2.getHeight= function(){
+    console.log(this.height);
+}
+
+person2.getHeight();
 
 ```
--- src/js/index.js
+++ src/js/index.js
@@ -10,20 +10,44 @@ const person ={
 console.log(typeof person);
 console.log(person)
 
-/** We can add the properties dynamically by using person.favColor ='white' */
-person.favColor= "white" 
-/** OR */
-/** We can add the properties dynamically by using person["favColor"] ='white' */
+/** We can add the properties dynamically by using person.favColor ='white'  or person["favColor"] ='white' */
+/** person.favColor= "white" */
 person["favColor"]= "yellow" 
 
-/** We can get the specific property by using person.fistName */
+/** We can get the specific property by using person.fistName or person["fistName"]  */
 console.log(person.firstName) 
-
-/** OR */
-/** We can get the specific property by using person["fistName"] */
 console.log(person["firstName"]) 
 console.log(person)
 
+/** To delete properties from object use delete person.favColor or delete person["favColor"] */
+/** delete person.favColor */
+delete person["favColor"]
+console.log(person)
+
 /** Example 01 creating object and adding properties*/
+
+
+/** Example 02 adding methods*/
+const person2 ={
+    firstName : 'Koseksi',
+    lastName :'P',
+    gender: 'Male',
+    height:'5.7',
+    fullName(){
+        console.log(`${this.firstName} ${this.lastName}`);
+    }
+}
+
+console.log(person2)
+person2.fullName();
+
+/** We can add methods from outside also by using reference type */
+person2.getHeight= function(){
+    console.log(this.height);
+}
+
+person2.getHeight();
+/** Example 02 adding methods*/
+
 
 
